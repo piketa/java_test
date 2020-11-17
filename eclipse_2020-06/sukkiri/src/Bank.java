cd3eb245a57c2e6363403343ec1e4916ae1a3697
piketa <noriaki.matsui@icloud.com>
2020-11-17 21:26:18 +0900

    20201117

---
+++ eclipse_2020-06/sukkiri/src/Bank.java
@@ -0,0 +1,10 @@
+import org.apache.commons.lang3.builder.EqualsBuilder;
+
+public class Bank {
+	String name;
+	String address;
+
+	public boolean equals(Object o) {
+		return EqualsBuilder.reflectionEquals(this, o);
+	}
+}
