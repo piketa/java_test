2b07f42d4f2cfe574ed7355d035420163fcef7f3
piketa <noriaki.matsui@icloud.com>
2022-09-16 08:36:11 +0900

    20220916

--- eclipse_2020-12/Test/src/Test.java
+++ eclipse_2020-12/Test/src/Test.java
@@ -2,10 +2,15 @@
 public class Test {
 
 	public static void main(String[] args) {
-		while(true) {
-			System.out.println("test");
-		}
+		int a = 10;
+//		int b = a++ + a + a-- - a-- + ++a;
+		int b = a++ + a + a-- - a-- + ++a;
+		System.out.println(b);
 
+
+		int a1 = 10;
+		int b1 = a1++ + a1;
+		System.out.println(b1);
 	}
 
 }
